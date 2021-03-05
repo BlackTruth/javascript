6d0676632ef287189396e59ae981bc49b6528365
Kanstantsin Kulahin <blacktruth1991@gmail.com>
2021-03-05 17:12:05 +0300

    feat: add draw table

--- hw_2/script.js
+++ hw_2/script.js
@@ -6,33 +6,34 @@ function randn_bm() {
     return (Math.sqrt(-2.0 * Math.log(u)) * Math.cos(2.0 * Math.PI * v) * 2) | 0;
 }
 
-function fillArrayWithRandom(count, randomFunc) {
-    let arr = [];
+function fillAndGroupRandom(count, randomFunc) {
+    const resultObject = {};
     let wCount = count + 1;
 
-    while (wCount -= 1) {
-        arr.push(randomFunc());
-    }
-    console.log(arr);
-    return arr;
-}
-
-function groupArray(arr) {
-    const resultObject = {};
-
-    arr.forEach((elem) => {
+    while ((wCount -= 1)) {
+        const elem = randomFunc();
         if (elem in resultObject) {
             resultObject[elem] += 1;
         } else {
             resultObject[elem] = 1;
         }
-    });
+    }
     return resultObject;
 }
 
-function groupRandom(count, randomFunc) {
-    const arr = fillArrayWithRandom(count, randomFunc);
-    return groupArray(arr);
+function drawTable(obj) {
+    console.log(obj);
+    console.log("+---------+---------+");
+    console.log("|  Number | Count   |");
+    console.log("+---------+---------+");
+    Object.getOwnPropertyNames(obj)
+        .sort((x, y) => x - y)
+        .forEach((prop) => {
+            console.log(
+                `|${String(prop).padStart(8)} | ${String(obj[prop]).padEnd(8)}|`
+            );
+        });
+    console.log("+---------+---------+");
 }
 
-console.log(groupRandom(10, randn_bm));
+drawTable(fillAndGroupRandom(10, randn_bm));
