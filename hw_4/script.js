430c7f711051339edb9251293fd83bd56d5a6cd9
Kanstantsin Kulahin <blacktruth1991@gmail.com>
2021-03-22 21:00:23 +0300

    mend

--- hw_4/script.js
+++ hw_4/script.js
@@ -7,7 +7,7 @@ var arr1 = [];
 function heapFilling() {
   for (let i = 0; i < 1024 * 1024 * 108; i++) {
     arr1.push(1);
-    // If you uncomment the next line of code and run in node.js, no heap overflow occurs... It's MAGIC!
+    // If you uncomment the next line of code and run in node.js, no heap overflow occurs...
     // I think when console.log is called, the engine optimizations have time to work.
     // console.log(process.memoryUsage().heapUsed);
   }
@@ -15,7 +15,7 @@ function heapFilling() {
 
 // Caution! Calling the next part of the code will result in a stack overflow.
 // try {
-//   console.log("A factorial of a 100000 will cause a stack overflow: ");
+//   console.log("Recursive function factorial of a 100000 will cause a stack overflow: ");
 //   console.log(factorial(100000));
 // } catch (e) {
 //   console.error(e.message);
